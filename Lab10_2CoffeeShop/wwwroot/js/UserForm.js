62405ca79cb0dafc4cc6f4607f7c42f712011888
Adam Rolain <AdamRolain@QuickenLoans.com>
2021-10-04 17:48:02 -0400

    Made create user form page and completed methods for validating First name, last name, and email addresses

---
+++ Lab10_2CoffeeShop/wwwroot/js/UserForm.js
@@ -0,0 +1,29 @@
+﻿let firstNameError = document.querySelector('#FirstNameFormGroup div');
+let lastNameError = document.querySelector('#LastNameFormGroup div');
+let emailError = document.querySelector('#EmailFormGroup div');
+let passwordError = document.querySelector('#PasswordFormGroup div');
+let confirmPasswordError = document.querySelector('#ConfirmPasswordFormGroup div');
+
+const displayFirstNameError = () => {
+    let input = document.getElementById('FirstName').value;
+    if (input.length <= 2 && input.length > 0) {
+        firstNameError.innerText = 'First name must be at least 2 characters';
+    }
+    firstNameError.classList.remove('text-hide');
+}
+
+const displayLastNameError = () => {
+    let input = document.getElementById('LastName').value;
+    if (input.length <= 2 && input.length > 0) {
+        lastNameError.innerText = 'Last name must be at least 2 characters';
+    }
+    lastNameError.classList.remove('text-hide');
+}
+
+const displayEmailError = () => {
+    let input = document.getElementById('Email').value;
+    let re = /.{2,}@.{2,}\..{2,}/;
+    if (!re.test(input)) {
+        emailError.classList.remove('text-hide');
+    }
+}
